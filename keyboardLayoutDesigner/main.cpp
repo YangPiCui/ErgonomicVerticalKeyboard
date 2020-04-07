6971bb82e8bacd37cd2d9fe69958e4558a3702e0
roundPi <yang.pi.cui@gmail.com>
2020-04-07 11:27:09 +0800

    push initial files

---
+++ keyboardLayoutDesigner/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+//Goal:
+//Keyboard layout to minimize fatigue and then maximize speed.
+//
+//Requirements :
+//	1. Comfortable in other languages
+//	2. Use a large sample of words
+//
+//	Targets, tools, backup plan :
+//1. Use C++
+//2. Publication quality
+//
+//Process Breakdown
+// Create enumerator classes to hold keyboard keys and fingers.
+//Assign keyscores, combination scores
+// Average sentence length 15-20 words.
+
+enum class IKeyBoard
+{
+
+};
+
+enum class Finger
+{
+
+};
+
+
+
+int main()
+{
+	return 0;
+}
+
+
